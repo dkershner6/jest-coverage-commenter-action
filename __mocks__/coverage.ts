e36c2134d76454707af503bcca8a79a4a41d7435
Derek Kershner <dkershner@cbtnuggets.com>
2020-06-13 11:41:21 -0700

    One more expectation fix

--- __mocks__/coverage.ts
+++ __mocks__/coverage.ts
@@ -9,9 +9,7 @@ All files |     100 |      100 |     100 |     100 |
  index.ts |     100 |      100 |     100 |     100 |                   
 ----------|---------|----------|---------|---------|-------------------`;
 
-export const simpleExpectation = `\`\`\`
-File      | % Stmts | % Branch | % Funcs | % Lines | Uncovered Line #s 
+export const simpleExpectation = `File      | % Stmts | % Branch | % Funcs | % Lines | Uncovered Line #s 
 ----------|---------|----------|---------|---------|-------------------
 All files |     100 |      100 |     100 |     100 |                   
- index.ts |     100 |      100 |     100 |     100 |                   
-\`\`\``;
+_index.ts |     100 |      100 |     100 |     100 |                   `;
